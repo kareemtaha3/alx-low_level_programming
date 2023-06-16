7c43be361fc03b53f21233f6db2f43afe1d542cc
kareemtaha3 <tahakareem27@gmail.com>
2023-06-16 14:00:01 +0000

    small and capital alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: print small and captital alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* your code goes there */
+	char c = 'a';
+
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		++c;
+	}
+	while( d <= 'Z')
+	{
+		putchar(d);
+		++d;
+	}
+	putchar('\n');
+
+	return (0);
+}

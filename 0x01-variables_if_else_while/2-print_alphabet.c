8e07041bb2f402dea043c2cceb5f6c1d17ddf44f
kareemtaha3 <tahakareem27@gmail.com>
2023-06-16 13:36:23 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: print alphabit characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* your code goes there */
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		++c;
+	}
+	return (0);
+}

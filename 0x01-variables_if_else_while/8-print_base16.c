51cb59f4446e49e8a11f615e38a1aaea2664edd0
kareemtaha3 <tahakareem27@gmail.com>
2023-06-16 14:44:10 +0000

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: print alphabit characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* your code goes there */
+	int n = 0;
+	char c = 'a';
+
+	while (n < 10)
+	{
+		putchar(n)
+		++n;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		++c;
+	}
+	putchar('\n');
+
+	return (0);
+}

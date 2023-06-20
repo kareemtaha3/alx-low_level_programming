4eff38b39f073f6262cdbc3a622762081ca54fdb
kareemtaha3 <tahakareem27@gmail.com>
2023-06-20 18:17:15 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit-function determine if lower case or upper case
+ * @n: is a parameter might be upper case or lower case
+ * Return: value of _islower is 1 if lower case and 0 if upper case
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+
+}

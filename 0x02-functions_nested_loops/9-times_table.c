332da4efee85f5fca34a1ddb8c863882dcc4e4b4
kareemtaha3 <tahakareem27@gmail.com>
2023-06-20 22:09:49 +0000

    tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 				_putchar(a * b + '0');
 			else
 			{
-				_putchar((a * b) % 100 + '0');
+				_putchar((a * b) / 10 + '0');
 				_putchar((a * b) % 10 + '0');
 			}
 			++b;

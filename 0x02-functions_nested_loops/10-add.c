1b79f029cc9cc62ee41495ecbd9340b5a12f8fd2
kareemtaha3 <tahakareem27@gmail.com>
2023-06-20 22:36:24 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add-function print the clock
+ *@a:parameter
+ *@b:parameter
+ *Return: return sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}

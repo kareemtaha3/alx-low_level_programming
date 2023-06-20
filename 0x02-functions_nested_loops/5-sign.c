a6d75dfc08c3953096cc3c647e159e3993d8cb1b
kareemtaha3 <tahakareem27@gmail.com>
2023-06-20 17:36:33 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_sign-function determine if lower case or upper case
+ * @n: is a parameter might be upper case or lower case
+ * Return: value of _islower is 1 if lower case and 0 if upper case
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+}

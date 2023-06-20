64b0fce33154357df003a7420dd58e7f9e0127ec
kareemtaha3 <tahakareem27@gmail.com>
2023-06-20 20:15:24 +0000

    signs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,14 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = (-1) * n;
+	int last_digit;
 
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	if (n < 0)
+		last_digit = ((-1) * n) % 10;
+	else
+		last_digit = n % 10;
+
+	_putchar((last_digit + '0';
+	return (last_digit);
 
 }

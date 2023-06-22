9553d60323790593ec0c155165b8c17a7d4cb871
kareemtaha3 <tahakareem27@gmail.com>
2023-06-22 22:08:16 +0000

    num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers-function check if letter is upper case or not
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		_putchar(i + '0');
+	_putchar('\n');
+}

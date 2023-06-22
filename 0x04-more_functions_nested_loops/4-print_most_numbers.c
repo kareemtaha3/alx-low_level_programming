1fb76795045f9c10fbf638117af9f66f7d7c2d93
kareemtaha3 <tahakareem27@gmail.com>
2023-06-22 22:29:35 +0000

    print: :

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers-function check if letter is upper case or not
+ *		only using _puhcar twice
+ * Return: Always 0 (Success)
+ */
+
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		if (num != 2 && num != 4)
+			_putchar(num + 48);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int _puhchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void pint_most_numbers(void);

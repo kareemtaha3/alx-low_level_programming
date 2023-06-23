7da65b0ddd675009929f246d32a9961c1438bc08
kareemtaha3 <tahakareem27@gmail.com>
2023-06-23 00:30:20 +0000

    try

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal-print line
+ * @n: parameter
+ * Return: Always 0 (Success)
+ */
+
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+
+
+
+
+
+
+
+}

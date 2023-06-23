13419fe94c8808f5e14c9eecc91041eb63b32ddc
kareemtaha3 <tahakareem27@gmail.com>
2023-06-23 01:57:32 +0000

    vi

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle-print line
+ * @size: parameter
+ * Return: Always 0 (Success)
+ */
+
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+	for (i = 0; i < size; ++i)
+	{
+		for (j = n - i - 1; j >= 0; ++i)
+			_putchar(' ');
+
+		for (j = 0; j < i + 1; ++j)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+	}
+
+
+
+
+
+
+}

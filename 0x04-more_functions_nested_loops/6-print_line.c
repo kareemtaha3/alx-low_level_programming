1b9aed2bd1fc6196ab7555ceb46090aa33a40bc1
kareemtaha3 <tahakareem27@gmail.com>
2023-06-23 00:54:28 +0000

    what

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
 /**
- * print_line-print line
- * @n: parameter
+ * print_square-print line
+ * @size: parameter
  * Return: Always 0 (Success)
  */
 
 
-void print_line(int n)
+void print_square(int size)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < n; ++i)
+	if (size <= 0)
+		_putchar('\n')
+	else
 	{
-		_putchar('_');
+	for (i = 0; i < size; ++i)
+	{
+		for (j = 0; j < size; ++j)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 
-
+	}
 
 
 

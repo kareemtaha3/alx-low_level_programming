9b0253c13e3c1f6d11d337b7ae4aabd5d94968ac
kareemtaha3 <tahakareem27@gmail.com>
2023-06-26 21:03:02 +0000

    0. 98 Battery st.:

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98-function to reset the value of  n to 98
+ * @n:pointer to integer
+ * Renturn: no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+int _putchar(char c);
+void rest_to_98(int *n);

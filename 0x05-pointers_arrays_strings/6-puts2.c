2d6cc7707b651c1f40b0c81f3ddae82ba1e765e1
kareemtaha3 <tahakareem27@gmail.com>
2023-06-27 00:45:06 +0000

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,24 @@
 #include "main.h"
+
+/**
+ *_strlen-function to count the length of the string
+ *@s:parameter
+ *Return:return an integer
+ */
+int _strlen(char *s)
+{
+	int count, i;
+
+	i = 0, count = 0;
+	while (s[i] != '\0')
+	{
+		++count;
+		++i;
+	}
+	return (count);
+}
+
+
 /**
  * puts2-function to print a string
  * @str:string
@@ -7,10 +27,11 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
 	i = 0;
-	while (str[i] != '\0')
+	len = _strlen(str);
+	while (i < len)
 	{
 		_putchar(str[i]);
 		i += 2;

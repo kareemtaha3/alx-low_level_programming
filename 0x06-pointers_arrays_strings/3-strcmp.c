6c41f8fa6899a781dd63f682d1ac15576ae6e696
kareemtaha3 <tahakareem27@gmail.com>
2023-06-29 20:46:51 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp-function to copy n char of the second string to the first string
+ * @s1:first string
+ * @s2:second string
+ * Return:return an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 > s2)
+		return (15);
+	else if (s1 < s2)
+		return (-15);
+	else
+		return (0);
+}

97c4cb46c1eadf27b82381291942dc4849783143
kareemtaha3 <tahakareem27@gmail.com>
2023-06-30 02:44:09 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,49 +1,31 @@
 #include "main.h"
-/**
- * is_separator-function to check if there is a separator char
- * @a:array of chars
- * Return: 0 or 1
- */
-
-
-int is_separator(char a)
-{
-	char separators[] = " \t\n,.!?\"(){}";
-	int i;
-
-	for (i = 0; i < 12; ++i)
-	{
-		if (a == separators[i])
-			return (1);
-		else
-			return (0);
-	}
-
-}
-
-
-
 
 /**
- * cap_string-function to reverse the element of the array
- * @a:array of chars
- * Return:array of chars
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
  */
-char *cap_string(char *a)
+char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; a[i] != '\0'; ++i)
+	while (*(s + i))
 	{
-		if (is_separator(a[i]))
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (a[i + 1] >= 97 && a[i + 1] <= 122)
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				a[i + 1] = a[i + 1] - 32;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
-
+		i++;
 	}
-
-	return (a);
+	return (s);
 }
